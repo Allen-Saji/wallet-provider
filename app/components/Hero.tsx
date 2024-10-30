3f05c605bbd5e16d9b4493f98c4f55020c54d641
Allen-Saji <allensaji04@gmail.com>
2024-10-30 17:36:09 +0530

    add redirection and loader

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DashboardButton } from "./Button";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -9,28 +9,37 @@ export default function Hero() {
   const router = useRouter();
   const userId = session?.user ? (session.user as { uid: string }).uid : null;
 
-  const handleDashboardRedirect = () => {
+  // State for loading status
+  const [loading, setLoading] = useState(false);
+
+  // Automatically redirect to dashboard if user exists
+  useEffect(() => {
     if (userId) {
+      setLoading(true); // Start loading
       router.push("/dashboard/" + userId);
     }
-  };
+  }, [userId, router]);
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-      <div className="text-4xl font-bold text-purple-300 mb-6">
-        Welcome to WalletEase
-      </div>
-      <div className="text-xl text-purple-200 mb-12">
-        Your Gateway to Effortless Crypto Management
-      </div>
-      {session?.user ? (
-        <DashboardButton onClick={handleDashboardRedirect}>
-          Go to Dashboard
-        </DashboardButton>
+      {loading ? (
+        <div className="text-xl font-semibold text-purple-200">
+          Redirecting...
+        </div>
       ) : (
-        <DashboardButton onClick={() => signIn("google")}>
-          Signup for WalletEase
-        </DashboardButton>
+        <>
+          <div className="text-4xl font-bold text-purple-300 mb-6">
+            Welcome to WalletEase
+          </div>
+          <div className="text-xl text-purple-200 mb-12">
+            Your Gateway to Effortless Crypto Management
+          </div>
+          {!session?.user && (
+            <DashboardButton onClick={() => signIn("google")}>
+              Signup for WalletEase
+            </DashboardButton>
+          )}
+        </>
       )}
     </div>
   );
